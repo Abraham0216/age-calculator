3eb0aeb7fd467655c4fe4d4d4e3ac83f70ce0ef2
Abraham Oñate <abrahamonate10@gmail.com>
2023-06-12 16:50:15 -0500

    Firebase added

---
+++ firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBHWNMq4cF4sd2hoTeynjFWPbmbs4ikP_8",
+  authDomain: "age-calculator-c82ee.firebaseapp.com",
+  projectId: "age-calculator-c82ee",
+  storageBucket: "age-calculator-c82ee.appspot.com",
+  messagingSenderId: "49452347240",
+  appId: "1:49452347240:web:9ef20ed103ef23fd9b70dd"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
